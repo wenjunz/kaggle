080e9f9419c614cf87db9f5bdcb1a11d4905b771
Wenjun Zhang <wenjunz@alice.mit.edu>
2015-05-20 09:56:41 -0400

    removed year, month and date as features, added week of day, improved the score to 0.42638

--- bike_sharing/random_forest.py
+++ bike_sharing/random_forest.py
@@ -3,9 +3,7 @@ import pandas as pd
 
 import pdb
 
-from sklearn.svm import SVC
-from sklearn import ensemble
-from sklearn.cross_validation import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.grid_search import GridSearchCV
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.decomposition.pca import PCA
@@ -18,6 +16,7 @@ train['year'] = pd.Series([train['datetime'][i].year for i in train.index], inde
 train['month'] = pd.Series([train['datetime'][i].month for i in train.index], index=train.index)
 train['day'] = pd.Series([train['datetime'][i].day for i in train.index], index=train.index)
 train['hour'] = pd.Series([train['datetime'][i].hour for i in train.index], index=train.index)
+train['weekday'] = pd.Series([train['datetime'][i].weekday() for i in train.index], index=train.index)
 train['log_casual'] = np.log(1+train['casual'])
 train['log_registered'] = np.log(1+train['registered'])
 
@@ -27,42 +26,37 @@ test['year'] = pd.Series([test['datetime'][i].year for i in test.index], index=t
 test['month'] = pd.Series([test['datetime'][i].month for i in test.index], index=test.index)
 test['day'] = pd.Series([test['datetime'][i].day for i in test.index], index=test.index)
 test['hour'] = pd.Series([test['datetime'][i].hour for i in test.index], index=test.index)
+test['weekday'] = pd.Series([test['datetime'][i].weekday() for i in test.index], index=test.index)
 
 feature_cols = [col for col in train.columns \
-        if col not in ['datetime','casual','registered','count','log_casual','log_registered']]
+        if col not in ['year','month','day','datetime','casual','registered','count','log_casual','log_registered']]
 
-X_train_casual, X_val_casual, y_train_casual, y_val_casual = train_test_split(\
-        train[feature_cols], train['log_casual'],test_size=0.2, random_state=0)
+#parameters = {'n_estimators':[100,500,1000,2000],\
+#        'min_samples_split':[1,5,10,20]}
 
-forest_casual = ensemble.RandomForestRegressor(n_estimators=1000, criterion='mse', \
-        max_depth=None, min_samples_split=11, min_samples_leaf=1, \
-        max_features='auto', bootstrap=True, oob_score=False, n_jobs=-1, \
-        random_state=None, verbose=0).fit(X_train_casual,y_train_casual)
-print('Validation Score is %f' %forest_casual.score(X_val_casual,y_val_casual))
-
-forest_registered = ensemble.RandomForestRegressor(n_estimators=1000, criterion='mse', \
-        max_depth=None, min_samples_split=11, min_samples_leaf=1, \
-        max_features='auto', bootstrap=True, oob_score=False, n_jobs=-1, \
-        random_state=None, verbose=0).fit(X_train_registered,y_train_registered)
-print('Validation Score is %f' %forest_registered.score(X_val_registered,y_val_registered))
-
-y_val = y_val_casual + y_val_registered;
-print('Validation Score is %f' %forest_registered.score(X_val,y_val))
-#mc = confusion_matrix(y_val,forest.predict(X_val))
-
-#print('Validation Score is %f' %forest.score(X_val,y_val_casual))
-
-#print(pd.DataFrame(forest.feature_importances_,index=feature_cols).sort([0], ascending=False)[:10])
-
-forest_casual.fit(train[feature_cols],train['log_casual'])
+# casual count
+#clf_casual = GridSearchCV(RandomForestRegressor(),parameters,n_jobs=2)
+#clf_casual.fit(train[feature_cols],train['log_casual'])
+#print(pd.DataFrame(clf_casual.best_estimator_.feature_importances_,index=feature_cols).sort([0], ascending=False)[:10])
+#cm_casual = confusion_matrix(train['log_casual'],clf_casual.predict(train[feature_cols]))
+#forest_casual = clf_casual.best_estimator_.fit(train[feature_cols],train['log_casual'])
+forest_casual = RandomForestRegressor(n_estimators=1000,min_samples_split=11).fit(train[feature_cols],train['log_casual'])
+print(pd.DataFrame(forest_casual.feature_importances_,index=feature_cols).sort([0], ascending=False)[:10])
 pred_casual = np.round(np.exp(forest_casual.predict(test[feature_cols]))-1)
 
-forest_registered.fit(train[feature_cols],train['log_registered'])
+# registered count
+#clf_registered = GridSearchCV(RandomForestRegressor(),parameters,n_jobs=2)
+#clf_registered.fit(train[feature_cols],train['log_registered'])
+#print(pd.DataFrame(clf_registered.best_estimator_.feature_importances_,index=feature_cols).sort([0], ascending=False)[:10])
+#cm_registered = confusion_matrix(train['log_registered'],clf_registered.predict(train[feature_cols]))
+#forest_registered = clf_registered.best_estimator_.fit(train[feature_cols],train['log_registered'])
+forest_registered = RandomForestRegressor(n_estimators=1000,min_samples_split=11).fit(train[feature_cols],train['log_registered'])
+print(pd.DataFrame(forest_registered.feature_importances_,index=feature_cols).sort([0], ascending=False)[:10])
 pred_registered = np.round(np.exp(forest_registered.predict(test[feature_cols]))-1)
+
 
 pred_count = pred_casual+pred_registered
 test['count'] = pred_count;
-
-test.to_csv('random_forest3.csv', cols=['datetime','count'],header=True,index=False)
+test.to_csv('random_forest6.csv', cols=['datetime','count'],header=True,index=False)
 
 
